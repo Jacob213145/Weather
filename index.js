6fa1f9f55742c401fdc47a58c054691e67980586
Jacob Jones <125416036+Jacob213145@users.noreply.github.com>
2023-03-08 00:28:48 +0000

    Exercise 2 – 3 points
    1.	Fork the weather with zip Repo from GitHub in https://github.com/debasisb/weather
    2.	Create a new Repl by Import from GitHub and name Repl weather-with-cityid
    3.	Go to Open Weather Map API - https://openweathermap.org/current
    4.	You will modify your code to query weather data by City ID = 2172797
    5.	Create one text fields in your HTML form for City ID
    6.	Ensure you use Units = Imperial and not the default Kelvin or Metric Celsius
    7.	Use your own API key that you used in class for previous assignments
    8.	Display the City Name, Temperature in Fahrenheit, Humidity in %
    9.	Display Wind Speed and Wind Direction (show correct units!)
    10.	Display Cloudiness in %
    11.	Run the entire code in Repl and Commit/Save the working Code in Github

--- index.js
+++ index.js
@@ -14,39 +14,42 @@ app.get("/", function(req, res) {
 //invoked after hitting go in the html form
 app.post("/", function(req, res) {
     
-    // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
-        var zip = String(req.body.zipInput);
-        console.log(req.body.zipInput);
+    // takes in the city id from the html form, display in console.
+    const cityId = req.body.cityIdInput;
+    console.log(cityId);
     
-    //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
-        const units = "imperial";
-        const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
-        const url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip +  "&units=" + units + "&APPID=" + apiKey;
+    //build up the URL for the JSON query, API Key is secret and needs to be obtained by signup 
+    const units = "imperial";
+    const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
+    const url = "https://api.openweathermap.org/data/2.5/weather?id=" + cityId + "&units=" + units + "&appid=" + apiKey;
     
-    // this gets the data from Open WeatherPI
+    // this gets the data from Open Weather API
     https.get(url, function(response){
         console.log(response.statusCode);
         
         // gets individual items from Open Weather API
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const city = weatherData.name;
-            const weatherDescription = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            const cityName = weatherData.name;
+            const temperature = weatherData.main.temp;
+            const humidity = weatherData.main.humidity;
+            const windSpeed = weatherData.wind.speed;
+            const windDirection = weatherData.wind.deg;
+            const cloudiness = weatherData.clouds.all;
             
             // displays the output of the results
-            res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " " + zip + " is " + temp + " Degrees Fahrenheit<h2>");
-            res.write("<img src=" + imageURL +">");
+            res.write("<h1>Weather in " + cityName + "</h1>");
+            res.write("<p>Temperature: " + temperature + " &#8457;</p>");
+            res.write("<p>Humidity: " + humidity + " %</p>");
+            res.write("<p>Wind Speed: " + windSpeed + " mph</p>");
+            res.write("<p>Wind Direction: " + windDirection + " &#176;</p>");
+            res.write("<p>Cloudiness: " + cloudiness + " %</p>");
             res.send();
         });
     });
-})
-
+});
 
 //Code will run on 3000 or any available open port
 app.listen(process.env.PORT || 3000, function() {
-console.log ("Server is running on port")
+  console.log("Server is running on port");
 });
